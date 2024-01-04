6029490a2fc873a2b064bcedf4060809f8b2d8bb
Hayden Dewey <hayden@deweys.net>
2024-01-04 12:42:06 -0600

    Updated link components with noreferrer tags and alt text

--- src/github-link/GitHubLink.js
+++ src/github-link/GitHubLink.js
@@ -3,7 +3,7 @@ import githubLogo from '../images/logos/github-mark-white.svg'
 
 export default function GitHubLink({ link }) {
     return (
-        <a className='github-link' href={link} target="_blank">
+        <a className='github-link' href={link} target="_blank" rel='noreferrer'>
             <img className='github-icon' src={githubLogo} alt='GitHub'/>
             View GitHub Repo
         </a>
--- src/itch-link/ItchLink.js
+++ src/itch-link/ItchLink.js
@@ -3,9 +3,9 @@ import itchLogo from '../images/logos/itchio-logo-white.svg';
 
 export default function ItchLink({ link }) {
     return (
-        <a className='itch-link' href={link} target='_blank'>
+        <a className='itch-link' href={link} target='_blank' rel='noreferrer'>
             View on
-            <img className='itch-icon' src={itchLogo}/>
+            <img className='itch-icon' src={itchLogo} alt='Itch.io'/>
         </a>
     );
 }
